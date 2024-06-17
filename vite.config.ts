e2b984aa7ea566e22768c994dbe3141de063f910
Jenny <isjia@163.com>
2024-06-17 08:11:11 +0800

    mod: env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
 // import { visualizer } from 'rollup-plugin-visualizer';
@@ -6,16 +6,20 @@ import legacy from '@vitejs/plugin-legacy';
 import pkg from './package.json';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-    base: mode === 'production' ? '/typing-jp/' : '/',
-    define: { __APP_VERSION__: JSON.stringify(pkg.version) },
-    plugins: [
-        react(),
-        legacy({
-            targets: ['defaults', 'not IE 11'],
-        }),
-        // visualizer({ open: true, brotliSize: true, filename: 'report.html' }),
-    ],
-    resolve: { alias: { '@': '/src' } },
-    build: { target: 'es2015' },
-}));
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        // base: mode === 'production' ? '/typing-jp/' : '/',
+        base: env.VITE_BASE_URL,
+        define: { __APP_VERSION__: JSON.stringify(pkg.version) },
+        plugins: [
+            react(),
+            legacy({
+                targets: ['defaults', 'not IE 11'],
+            }),
+            // visualizer({ open: true, brotliSize: true, filename: 'report.html' }),
+        ],
+        resolve: { alias: { '@': '/src' } },
+        build: { target: 'es2015' },
+    };
+});
